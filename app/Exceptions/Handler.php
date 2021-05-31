f7acf0621ca8c0d40854c134223dac1065371e78
Fajar Siagian <fajar7xx@gmail.com>
2021-05-31 09:41:22 +0700

    lanjutan untuk di rumah besok

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,17 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+     * @param  Throwable  $exception
+     * @throws Throwable
+     */
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
